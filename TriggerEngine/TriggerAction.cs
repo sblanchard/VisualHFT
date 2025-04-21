89d1dc71a6486ed3264c904ed55bf3e573394ab3
Prakash Sharma <prakash.sharma@superplum.com>
2025-04-21 12:41:32 +0530

    added discription on Cooldown Logic.

--- TriggerEngine/TriggerAction.cs
+++ TriggerEngine/TriggerAction.cs
@@ -18,6 +18,11 @@ namespace VisualHFT.TriggerEngine
         public RestApiAction? RestApi { get; set; }         // Only required if Type == RestApi
                                                             // Future: Add more actions (e.g., UIAlertAction, PluginCommandAction)
 
+
+        //Cooldown Logic
+        //Each action has its own cooldown(CooldownDuration and CooldownUnit).//
+        //The cooldown ensures that an action is not triggered too frequently, based on the duration and unit specified in the action(e.g., once per hour, once per day, etc.).
+
         public int CooldownDuration { get; set; } = 0;
         public TimeWindowUnit CooldownUnit { get; set; } = TimeWindowUnit.Seconds;
     }
